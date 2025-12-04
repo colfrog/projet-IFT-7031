f23f523f8bc8994903462a15becc124e428dc859
Laurent Cimon <Laurent.Cimon.1@ulaval.ca>
2025-12-03 21:53:51 -0500

    Add back quantization to save vram

--- qwen2-finetuning.py
+++ qwen2-finetuning.py
@@ -6,8 +6,10 @@ from transformers import (
     Qwen2AudioForConditionalGeneration,
     AutoProcessor,
     Trainer,
-    TrainingArguments
+    TrainingArguments,
+    BitsAndBytesConfig
 )
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 import os
 
 MODEL_ID = "Qwen/Qwen2-Audio-7B-Instruct"
@@ -15,15 +17,42 @@ DATA_FILE = "train_data.json"
 OUTPUT_DIR = "./qwen2-audio-finetuned"
 MODEL_PATH = os.path.abspath("/home/lcimon/scratch/hub/models--Qwen--Qwen2-Audio-7B-Instruct/snapshots/0a095220c30b7b31434169c3086508ef3ea5bf0a/")
 
+print(f"CUDA Version: {torch.version.cuda}")
+
 print("Loading model and processor... ", end="")
 print(f"({MODEL_PATH})" if os.path.exists(MODEL_PATH) else f"({MODEL_ID})")
+
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
 
 processor = AutoProcessor.from_pretrained(MODEL_PATH if os.path.exists(MODEL_PATH) else MODEL_ID)
 
 model = Qwen2AudioForConditionalGeneration.from_pretrained(
     MODEL_PATH if os.path.exists(MODEL_PATH) else MODEL_ID,
-    device_map="auto"
+    device_map="auto",
+    quantization_config=bnb_config,
+    attn_implementation="flash_attention_2"
 )
+
+# Prepare model for LoRA
+model = prepare_model_for_kbit_training(model)
+
+# Define LoRA Config
+peft_config = LoraConfig(
+    r=64, # Rank
+    lora_alpha=16,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+model = get_peft_model(model, peft_config)
+model.print_trainable_parameters()
 
 def load_data(json_file):
     with open(json_file, 'r') as f:
@@ -38,13 +67,13 @@ audios = []
 count = 0
 for sample in raw_data:
     print(f"\r{count}/{len(raw_data)}", end="")
-    audio, _ = librosa.load(sample.pop('audio'), sr=processor.feature_extractor.sampling_rate)
+    audio, _ = librosa.load(sample['audio'], sr=processor.feature_extractor.sampling_rate)
     audios.append(audio)
     count += 1
 print()
 
 text = processor.apply_chat_template(raw_data, add_generation_prompt=True, tokenize=False)
-inputs = processor(text=text, audios=audios, return_tensors="pt", padding=True)
+inputs = processor(text=text, audio=audios, return_tensors="pt", padding=True)
 dataset = Dataset.from_dict(inputs)
 
 # --- 4. Training Arguments ---
@@ -56,6 +85,7 @@ training_args = TrainingArguments(
     learning_rate=1e-4,
     num_train_epochs=1,
     logging_steps=10,
+    fp16=True,
     save_strategy="epoch",
     save_total_limit=1,
     report_to="none",
