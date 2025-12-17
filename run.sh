36722f2632707669a431e67a67208001c74611d9
Laurent Cimon <Laurent.Cimon.1@ulaval.ca>
2025-12-17 18:31:59 -0500

    Raise batch memory to 64GB to make sure the evaluation works

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1                # Number of tasks (see below)
 #SBATCH --nodes=1                 # Ensure that all cores are on one machine
 #SBATCH --time=0-23:59            # Runtime in D-HH:MM
-#SBATCH --mem=24000               # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --mem=64000               # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH --output=/home/lcimon/scratch/slurm_logs/%j.out  # File to which STDOUT will be written
 #SBATCH --error=/home/lcimon/scratch/slurm_logs/%j.err   # File to which STDERR will be written
 #SBATCH --gres=gpu:1              # Request one GPU
@@ -15,7 +15,6 @@ source ~/.bashrc
 source .venv/bin/activate
 export HF_HOME=/home/lcimon/scratch
 export KAGGLEHUB_CACHE=/home/lcimon/scratch/kaggle
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 python qwen2-finetuning.py
 
